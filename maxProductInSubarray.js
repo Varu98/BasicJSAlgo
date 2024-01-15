1d27cc9fcb9d42735aabd48176461d42fec6339d
varu98 <vardaan.agarwal.98@gmail.com>
2024-01-15 19:51:31 +0530

    refactor : max product subarray

--- maxProductInSubarray.js
+++ maxProductInSubarray.js
@@ -6,14 +6,14 @@ function maxProductSubarray(arr) {
   let prefix = 1;
   let suffix = 1;
   let ans = 0;
-  let n = arr.length;
+  let n = arr.length - 1;
 
-  for (let i = 0; i < n - 1; i++) {
+  for (let i = 0; i < n; i++) {
     if (prefix == 0) prefix = 1;
     if (suffix == 0) suffix = 1;
 
     prefix = arr[i] * prefix;
-    suffix = arr[n - 1 - i] * suffix;
+    suffix = arr[n - i] * suffix;
     ans = Math.max(ans, Math.max(suffix, prefix));
   }
 
